0d923bd67fbca136302d63752ac960bf33f8d233
rouzbeh hatamy <r.hatamy@toman.ir>
2024-06-26 12:24:47 +0330

    feat: add validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, CommonModule],
   controllers: [],
   providers: [],
 })
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,30 @@
+import {
+  IsEmail,
+  IsPhoneNumber,
+  IsString,
+  Length,
+  Matches,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @Length(2, 40)
   readonly name: string;
+
+  @IsEmail()
   readonly email: string;
+
+  @IsPhoneNumber('IR')
   readonly phone: string;
+
+  @IsString()
+  @Length(4, 20)
+  // Passwords will contain at least 1 upper case letter
+  // Passwords will contain at least 1 lower case letter
+  // Passwords will contain at least 1 number or special character
+  // There is no length validation (min, max) in this regex!
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is weak',
+  })
   readonly password: string;
 }
